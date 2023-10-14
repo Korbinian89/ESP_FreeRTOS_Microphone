65965d972fb244a836eb200be821977838be514c
KP <kpfaffeneder@web.de>
2023-10-14 22:45:36 +0200

    Save

--- src/sampler/adc_sampler.h
+++ src/sampler/adc_sampler.h
@@ -12,7 +12,7 @@ public:
     AdcSampler(adc_unit_t iAdcUnit, adc1_channel_t iAdcChannel, i2s_port_t iI2sPort, const i2s_config_t& iI2sConfig);
 
     int read(int16_t* samples, int count) override;
-    inline size_t write(int16_t* samples, int count) override { /* not used here */}
+    inline size_t write(int16_t* samples, int count) override { /* not used here */ return 0; }
 
 
 protected:
